8ea5cfa204403d078521a0eda7fead43d2bd2723
jawakow <jawakow@gmail.com>
2019-08-15 11:24:46 +0100

    array mode in csv file not implemented yet

---
+++ DDREBOND/S_Classes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace DD_Beyond
+{
+    public class S_Classes
+    {
+
+        string character;
+        string hit_Die;
+        string primaryAbility;
+        string[] saves;
+        string imagePath;
+
+        public S_Classes(string character, string hit_Die, string primaryAbility, string[] saves, string imagePath)
+        {
+            this.character = character;
+            this.hit_Die = hit_Die;
+            this.primaryAbility = primaryAbility;
+            this.saves = saves;
+            this.imagePath = imagePath;
+        }
+
+
+    }
+}
